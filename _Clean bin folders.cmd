6c04cbe225ace9366404ac6292f577452213b11d
Elektro <ElektroStudios@ElHacker.Net>
2026-01-11 14:18:04 +0100

    Create _Clean bin folders.cmd

---
+++ _Clean bin folders.cmd
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+:: Debug folder
+ECHO:[.] Cleaning Debug folder...
+(
+	RD  /Q /S ".\QobuzDownloaderX\bin\Debug\logs"
+	RD  /Q /S ".\QobuzDownloaderX\bin\Debug\qbdlx-temp"
+	DEL /F /Q ".\QobuzDownloaderX\bin\Debug\Latest_Error.log"
+	DEL /F /Q ".\QobuzDownloaderX\bin\Debug\post_template.txt"
+)>NUL 2>&1
+ECHO:[+] Debug folder has been cleaned.
+
+:: Release folder
+ECHO:[.] Cleaning Release folder...
+(
+	RD  /Q /S ".\QobuzDownloaderX\bin\Release\logs"
+	RD  /Q /S ".\QobuzDownloaderX\bin\Release\qbdlx-temp"
+	DEL /F /Q ".\QobuzDownloaderX\bin\Release\Latest_Error.log"
+	DEL /F /Q ".\QobuzDownloaderX\bin\Release\post_template.txt"
+	DEL /F /Q ".\QobuzDownloaderX\bin\Release\Newtonsoft.Json.xml"
+	DEL /F /Q ".\QobuzDownloaderX\bin\Release\*.pdb"
+)>NUL 2>&1
+ECHO:[+] Release folder has been cleaned.
+
+ECHO+
+PAUSE
+EXIT
