7f316adfb7a388b11d2b1ce5d56a8e8ddb51217c
Elektro <ElektroStudios@ElHacker.Net>
2026-01-11 11:05:48 +0100

    Update AssemblyInfo.cs

--- QobuzDownloaderX/Properties/AssemblyInfo.cs
+++ QobuzDownloaderX/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.4")]
-[assembly: AssemblyFileVersion("1.1.0.4")]
+[assembly: AssemblyVersion("1.1.0.5")]
+[assembly: AssemblyFileVersion("1.1.0.5")]
 [assembly: NeutralResourcesLanguage("en")]
