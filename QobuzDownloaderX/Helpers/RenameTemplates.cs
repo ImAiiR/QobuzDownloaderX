34438eb420532b939f820f2d335c42a4bb87060d
Elektro <ElektroStudios@ElHacker.Net>
2026-01-11 11:06:47 +0100

    Update RenameTemplates.cs

--- QobuzDownloaderX/Helpers/RenameTemplates.cs
+++ QobuzDownloaderX/Helpers/RenameTemplates.cs
@@ -8,7 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Windows.Forms;
 using ZetaLongPaths;
 
 namespace QobuzDownloaderX.Helpers
