9428d9e165344dedb94504e9794346efe894f06e
Elektro <ElektroStudios@ElHacker.Net>
2026-01-12 17:38:44 +0100

    Update PerformersParser.cs

--- QobuzDownloaderX/Helpers/QobuzDownloaderXMOD/PerformersParser.cs
+++ QobuzDownloaderX/Helpers/QobuzDownloaderXMOD/PerformersParser.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Windows.Forms;
 
 namespace QobuzDownloaderX.Helpers.QobuzDownloaderXMOD
 {
