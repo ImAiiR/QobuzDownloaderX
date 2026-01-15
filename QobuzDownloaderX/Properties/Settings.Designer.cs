c540a0d74b6655b06febe3f2b5209e10ac27f6a6
Elektro <ElektroStudios@ElHacker.Net>
2026-01-15 04:46:30 +0100

    Update Settings.Designer.cs

--- QobuzDownloaderX/Properties/Settings.Designer.cs
+++ QobuzDownloaderX/Properties/Settings.Designer.cs
@@ -9,8 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace QobuzDownloaderX.Properties {
-    
-    
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
