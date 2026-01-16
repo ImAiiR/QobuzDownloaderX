35c6a3936f4d2a8629e558f3e2d1edf107265b71
Elektro <ElektroStudios@ElHacker.Net>
2026-01-16 20:43:39 +0100

    Update _Clean bin folders.cmd

--- _Clean bin folders.cmd
+++ _Clean bin folders.cmd
@@ -7,16 +7,8 @@ ECHO:[.] Cleaning Debug folder...
 	RD  /Q /S ".\QobuzDownloaderX\bin\Debug\qbdlx-temp"
 	DEL /F /Q ".\QobuzDownloaderX\bin\Debug\Latest_Error.log"
 	DEL /F /Q ".\QobuzDownloaderX\bin\Debug\post_template.txt"
-	
-	(
-	echo ^<?xml version="1.0" encoding="utf-8"?^>
-	echo ^<settings^>
-	echo   ^<currentLanguage^>EN^</currentLanguage^>
-	echo   ^<currentTheme^>Titanium^</currentTheme^>
-	echo ^</settings^>
-	)> ".\QobuzDownloaderX\bin\Debug\user.config"
-
 )>NUL 2>&1
+CALL :ResetUserConfig ".\QobuzDownloaderX\bin\Debug\user.config"
 ECHO:[+] Debug folder has been cleaned.
 
 :: Release folder
@@ -28,18 +20,28 @@ ECHO:[.] Cleaning Release folder...
 	DEL /F /Q ".\QobuzDownloaderX\bin\Release\post_template.txt"
 	DEL /F /Q ".\QobuzDownloaderX\bin\Release\Newtonsoft.Json.xml"
 	DEL /F /Q ".\QobuzDownloaderX\bin\Release\*.pdb"
-
-	(
-	echo ^<?xml version="1.0" encoding="utf-8"?^>
-	echo ^<settings^>
-	echo   ^<currentLanguage^>EN^</currentLanguage^>
-	echo   ^<currentTheme^>Titanium^</currentTheme^>
-	echo ^</settings^>
-	)> ".\QobuzDownloaderX\bin\Release\user.config"
-
 )>NUL 2>&1
+CALL :ResetUserConfig ".\QobuzDownloaderX\bin\Release\user.config"
 ECHO:[+] Release folder has been cleaned.
 
 ECHO+
 PAUSE
 EXIT
+
+:ResetUserConfig
+DEL /Q "%~1"
+IF EXIST "%~1" (
+	COLOR 0C
+    ECHO: Error deleting "%~1"
+    PAUSE
+    EXIT /B
+)
+
+(
+	echo ^<?xml version="1.0" encoding="utf-8"?^>
+	echo ^<settings^>
+	echo   ^<currentLanguage^>EN^</currentLanguage^>
+	echo   ^<currentTheme^>Titanium^</currentTheme^>
+	echo ^</settings^>
+) > "%~1"
+EXIT /B
